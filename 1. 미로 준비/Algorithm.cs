c77e45c8c7f0ecbb59df9dfec4ac8a2f00679063
Festison <105289311+Festison@users.noreply.github.com>
2023-03-21 14:43:11 +0900

    Add files via upload

---
+++ 1. 미로 준비/Algorithm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp_Algorithm
+{
+    class Algorithm
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                // 만약에 경과한 시간이 1/30초보다 작다면
+                if (currentTick-lastTick < WAIT_TICK) 
+                {
+                    continue;
+                }
+
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+                
+                // 로직
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
---
+++ 1. 미로 준비/Board.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Algorithm
+{
+    
+
+    class Board
+    {
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public TileType[,] _tile; // 배열
+        public int _size;
+        const char CIRCLE = '\u25cf';
+
+        public void Initialize(int size)
+        {
+            if (size % 2 == 0)
+            {
+                return;
+            }
+
+            _tile = new TileType[size, size];
+            _size = size;
+
+            GenerateByBinaryTree();
+        }
+
+        void GenerateByBinaryTree()
+        {
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        _tile[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        _tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        continue;
+                    }
+                    if (y == _size - 2 && x == _size - 2)
+                    {
+                        continue;
+                    }
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (y == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+        }
+    }
+}
