3759417f4d9df5a3c9070053c12b41d2b73a3b3d
Festison <105289311+Festison@users.noreply.github.com>
2023-03-22 16:24:24 +0900

    Add files via upload

--- 1. 미로 준비/Algorithm.cs
+++ 1. 미로 준비/Algorithm.cs
@@ -1,41 +1,41 @@
 ﻿using System;
 
-namespace CSharp_Algorithm
+namespace Algorithm
 {
-    class Algorithm
-    {
-        static void Main(string[] args)
-        {
-            Board board = new Board();
-            board.Initialize(25);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Board board = new Board();
+			Player player = new Player();
+			board.Initialize(25, player);
+			player.Initialize(1, 1, board);
 
-            Console.CursorVisible = false;
+			Console.CursorVisible = false;
 
-            const int WAIT_TICK = 1000 / 30;
+			const int WAIT_TICK = 1000 / 30;			
 
-            int lastTick = 0;
+			int lastTick = 0;
+			while (true)
+			{
+				#region 프레임 관리
+				// 만약에 경과한 시간이 1/30초보다 작다면
+				int currentTick = System.Environment.TickCount;				
+				if (currentTick - lastTick < WAIT_TICK)
+					continue;
+				int deltaTick = currentTick - lastTick;
+				lastTick = currentTick;
+				#endregion
 
-            while (true)
-            {
-                #region 프레임 관리
-                int currentTick = System.Environment.TickCount;
-                // 만약에 경과한 시간이 1/30초보다 작다면
-                if (currentTick-lastTick < WAIT_TICK) 
-                {
-                    continue;
-                }
+				// 입력
 
-                lastTick = currentTick;
-                #endregion
+				// 로직
+				player.Update(deltaTick);
 
-                // 입력
-                
-                // 로직
-
-                // 렌더링
-                Console.SetCursorPosition(0, 0);
-                board.Render();
-            }
-        }
-    }
+				// 렌더링
+				Console.SetCursorPosition(0, 0);
+				board.Render();
+			}
+		}
+	}
 }
