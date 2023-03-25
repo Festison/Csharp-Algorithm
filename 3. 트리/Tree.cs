4e360a4ea3c1db2bd3f4ade25866698ef244ed56
Festison <105289311+Festison@users.noreply.github.com>
2023-03-25 14:16:33 +0900

    Add files via upload

---
+++ 3. 트리/Tree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise
+{
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+    }
+
+    class Tree
+    {
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+            }
+            return root;
+        }
+
+        static void PrintTree(TreeNode<string> root)
+        {
+            // 접근
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> child in root.Children)
+            {
+                // 재귀 함수 이용
+                PrintTree(child);
+            }
+        }
+
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+
+            foreach (TreeNode<string> child in root.Children)
+            {
+                int newHeight = GetHeight(child) + 1;
+                if (height < newHeight)
+                {
+                    height = newHeight;
+                }
+            }
+
+            return height;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode<string> root = MakeTree();
+
+            PrintTree(root);
+
+            Console.WriteLine(GetHeight(root));
+        }
+    }
+}
